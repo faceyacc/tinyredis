baad6ae8a1ac2ce85ae38fea8d6053bb54d45187
faceyacc <justfacey@gmail.com>
2024-08-12 13:45:04 -0400

    Built functionality to handle edge cases where an arry may have None values.

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -9,17 +9,54 @@ from tinyredis.protocol import *
 
 
 @pytest.mark.parametrize("buffer, expected", [
+    # Test invalid messages
+    (b"PING\r\n", (None, 0)),
+
+    # Test cases for Simple Strings
     (b"+Par", (None, 0)),
-    (b"+Ok\r\n", (SimpleString("Ok"), 5)),
-    (b"+Ok\r\n+Go\r\n", (SimpleString("Ok"), 5)),
-    (b"-Error message\r\n", (SimpleError("Error message"), 16)),
-    (b":0\r\n", (Integer(0), 4)),
-    (b":1000\r\n", (Integer(1000), 7)),
+    (b"+OK\r\n", (SimpleString("OK"), 5)),
+    (b"+OK\r\n+Next", (SimpleString("OK"), 5)),
+
+    # Test cases for Errors
+    (b"-Err", (None, 0)),
+    (b"-Error Message\r\n", (SimpleError("Error Message"), 16)),
+    (b"-Error Message\r\n+Other", (SimpleError("Error Message"), 16)),
+
+    # Test cases for Integers
+    (b":10", (None, 0)),
+    (b":100\r\n", (Integer(100), 6)),
+    (b":100\r\n+OK", (Integer(100), 6)),
+
+    # Test cases for Bulk Strings
+    (b"$5\r\nHel", (None, 0)),
+    (b"$5\r\nHello\r\n", (BulkString(b"Hello"), 11)),
+    (b"$12\r\nHello, World\r\n", (BulkString(b"Hello, World"), 19)),
+    (b"$12\r\nHello\r\nWorld\r\n", (BulkString(b"Hello\r\nWorld"), 19)),
     (b"$0\r\n\r\n", (BulkString(b""), 6)),
-    (b"$5\r\nhello\r\n", (BulkString(b"hello"), 11)),
-    (b"*3\r\n:1\r\n:2\r\n:3\r\n", (Array([Integer(1),Integer(2), Integer(3)]), 16)),
-    (b"*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n", (Array([BulkString(b"hello"), BulkString(b"world")]), 26)),
-    (b"*0\r\n", (Array([]), 4))
+    (b"$-1\r\n", (BulkString(None), 5)),
+
+    # Test case for Arrays
+    (b"*0", (None, 0)),
+    (b"*0\r\n", (Array([]), 4)),
+    (b"*-1\r\n", (Array(None), 5)),
+    (b"*2\r\n$5\r\nhello\r\n$5\r\n", (None, 0)),
+    (
+        b"*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n",
+        (Array([BulkString(b"hello"), BulkString(b"world")]), 26),
+    ),
+    (
+        b"*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n+OK",
+        (Array([BulkString(b"hello"), BulkString(b"world")]), 26),
+    ),
+    (b"*3\r\n:1\r\n:", (None, 0)),
+    (
+        b"*3\r\n:1\r\n:2\r\n:3\r\n",
+        (Array([Integer(1), Integer(2), Integer(3)]), 16),
+    ),
+    (
+        b"*3\r\n:1\r\n:2\r\n:3\r\n+OK",
+        (Array([Integer(1), Integer(2), Integer(3)]), 16),
+    ),
 ])
 
 
--- tinyredis/protocol.py
+++ tinyredis/protocol.py
@@ -4,7 +4,7 @@ MESSAGE_SEPARATOR = b"\r\n"
 
 @dataclass
 class BulkString:
-    data: bytes
+    data: bytes | None
 
 @dataclass
 class SimpleString:
@@ -20,38 +20,55 @@ class SimpleError:
 
 @dataclass
 class Array:
-    data: list[Integer|SimpleString|BulkString]
+    data: list[Integer|SimpleString|BulkString] | None
 
 def extract_frame_from_buffer(buffer: bytes):
+
+    if MESSAGE_SEPARATOR not in buffer:
+        return None, 0
+
     match chr(buffer[0]):
-        case '+':
+        case '+': # Simple Strings
             sep = buffer.find(MESSAGE_SEPARATOR)
             if sep != -1:
                 return SimpleString(buffer[1:sep].decode()), sep + 2
-        case '-':
+        case '-': # Errors
             sep = buffer.find(MESSAGE_SEPARATOR)
             if sep != -1:
                 return SimpleError(buffer[1:sep].decode()), sep + 2
-        case ':':
+        case ':': # Integers
             sep = buffer.find(MESSAGE_SEPARATOR)
             if sep != -1:
                 return Integer(int(buffer[1:sep])), sep + 2
-        case '$':
-            sep = buffer.find(MESSAGE_SEPARATOR)
-            length = int(buffer[1:sep])
-            data = buffer[sep + 2:sep + 2 + length]
-            if len(data) == length:
-                return BulkString(data), sep + 2 + length + 2
-
-        case '*':
-            arr = []
+        case '$': # Bulk strings
             sep = buffer.find(MESSAGE_SEPARATOR)
             array_length = int(buffer[1:sep])
+            data = buffer[sep + 2:sep + 2 + array_length]
+
+            if array_length == -1: # Null Bulk String
+                return BulkString(None), sep + 2
+
+            if len(data) == array_length:
+                return BulkString(data), sep + 2 + array_length + 2
+
+        case '*': # Arrays
+            arr = []
+
+            sep = buffer.find(MESSAGE_SEPARATOR)
+            array_length = int(buffer[1:sep])
+
+            # Null Array
+            if array_length == -1:
+                return Array(None), sep + 2
 
             for _ in range(array_length):
                 frame, offset = extract_frame_from_buffer(buffer[sep + 2:])
                 arr.append(frame)
                 sep += offset
+
+            # Return None if any element in the array is None
+            if None in arr:
+                return None, 0
 
             return Array(arr), sep + 2
 
